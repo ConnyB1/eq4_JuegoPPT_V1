49d9e2d609e4af839a0407e63b4d9d01372276df
Alejandro Velazquez <alejandrovelazquezmercado21@gmail.com>
2023-08-28 14:17:37 -0700

    Changes to be committed: 	new file:   eq4_JuegoPPT_V1.cpp 	new file:   output/eq4_JuegoPPT_V1.exe

---
+++ eq4_JuegoPPT_V1.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main()
+{
+    int op, comp;
+    srand(time(NULL));
+    printf("Selecciona una opcion: ");
+    printf("\n1)piedra");
+    printf("\n2)papel");
+    printf("\n3)tijera\n");
+    scanf("%d",&op);
+    comp=rand()%3;
+    printf("comp dice: %d\n",comp);
+    if((op == 1 && comp == 3) || (op == 2 && comp == 1))
+        {
+            printf("Ganaste");
+        }
+    if(op == 3 && comp == 2)
+        {
+            printf("Ganaste");
+        }
+    if((op == 1 && comp == 2) || (op == 2 && comp == 3))
+        {
+            printf("Perdiste");
+        }
+    if((op == 3 && comp == 1) || op == comp)
+        {
+            if(op == comp)
+                {
+                    printf("Empate");
+                }
+            else 
+                {
+                    printf("Perdiste"); 
+                }
+        }
+    return 0;
+}
